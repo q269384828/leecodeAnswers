8e7b8a71c129672c96557d9dbbe71087ec907841
APPLE <apple@APPLEdeMacBook-Pro.local>
2020-01-22 18:04:37 +0800

    提交

---
+++ code/148.排序链表.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=javascript
+ *
+ * [148] 排序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+    // 两种方式
+    // 1. 方式一:
+    
+    // 2. 方式二: 
+
+}
+// @lc code=end
