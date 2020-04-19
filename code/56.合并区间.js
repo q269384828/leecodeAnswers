72e0750d409e31c95e01cca60cbb4feebbdb394b
q269384828 <269384828@@qq.com>
2020-04-19 22:55:08 +0800

    tijiao

--- code/56.合并区间.js
+++ code/56.合并区间.js
@@ -5,40 +5,71 @@
  */
 
 // @lc code=start
+
+function doMerge (intervals) {
+  let aIdx = 0
+  const ret = []
+  console.log(intervals)
+
+  while (aIdx < (intervals.length - 1)) {
+    const a = intervals[aIdx]
+    const b = intervals[aIdx + 1]
+    if (a[1] >= b[0]) {
+      ret.push([a[0], Math.max(b[1], a[1])])
+      aIdx += 2
+    } else {
+      ret.push([a[0], a[1]])
+      aIdx += 1
+    }
+  }
+  if (aIdx < intervals.length) {
+    ret.push(intervals[aIdx])
+    aIdx++
+  }
+
+  //
+  let i = 0
+  let isFinal = true
+  while (i < ret.length - 1) {
+    if (ret[i][1] >= ret[i + 1][0]) {
+      isFinal = false
+      break
+    }
+    i++
+  }
+  if (!isFinal) {
+    return doMerge(ret)
+  }
+  return ret
+}
+
 /**
  * @param {number[][]} intervals
  * @return {number[][]}
  */
 var merge = function (intervals) {
-  const len = intervals.length
+  // const len = intervals.length
 
-  if (len < 2) {
-    return intervals
-  }
+  // if (len < 2) {
+  //   return intervals
+  // }
+  // intervals.sort((a, b) => a[0] - b[0])
+  // return doMerge(intervals)
+  const merged = []
   intervals.sort((a, b) => a[0] - b[0])
-  let aIdx = 0
-  let scan = 0
-  const ret = []
-  while ((aIdx + scan) < len) {
-    const a = intervals[aIdx]
-    const b = intervals[aIdx + scan]
-    if (a[1] >= b[0]) {
-      scan++
+  for (let index = 0; index < intervals.length; index++) {
+    const current = intervals[index]
+    if (merged.length === 0 || current[0] > merged[merged.length - 1][1]) {
+      merged.push(current)
     } else {
-      const left = a[0]
-
-      const right = Math.max(a[1], intervals[aIdx + scan - 1][1])
-      ret.push([left, right])
-      aIdx += scan
-      scan = 0
+      merged[merged.length - 1][1] = Math.max(current[1], merged[merged.length - 1][1])
     }
   }
-  if (scan) {
-    const left = intervals[aIdx][0]
-    const right = Math.max(intervals[aIdx][1], intervals[len - 1][1])
-    ret.push([left, right])
-  }
-  return ret
+  return merged
 }
 
 // @lc code=end
+
+module.exports = {
+  merge
+}
---
+++ test/56.合并区间_test.js
@@ -0,0 +1,6 @@
+const { merge } = require('../code/56.合并区间')
+const { test } = require('../helper')
+
+// test(merge, [[1, 4], [0, 2], [3, 5]])
+
+test(merge, [[5, 5], [1, 3], [3, 5], [4, 6], [1, 1], [3, 3], [5, 6], [3, 3], [2, 4], [0, 0]])
